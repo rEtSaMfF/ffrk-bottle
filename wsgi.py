95e9f917f744e54bec199ce14227d59486fe8e4e
rEtSaMfF <retsamff@gmail.com>
2015-05-27 17:33:58 -0400

    Add/remove os.path.join

--- wsgi.py
+++ wsgi.py
@@ -10,12 +10,13 @@ sys.path.insert(0, os.path.dirname(__file__) or '.')
 
 PY_DIR = os.path.join(os.environ['OPENSHIFT_HOMEDIR'], "python")
 
-virtenv = PY_DIR + '/virtenv/'
+virtenv = os.path.join(PY_DIR, 'virtenv')
 
 PY_CACHE = os.path.join(
     virtenv, 'lib', os.environ['OPENSHIFT_PYTHON_VERSION'], 'site-packages')
 
-os.environ['PYTHON_EGG_CACHE'] = os.path.join(PY_CACHE)
+os.environ['PYTHON_EGG_CACHE'] = PY_CACHE
+
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
 
 try:
